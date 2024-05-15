11f3fad361e31b882fa72dc292b16cce4ff7614f
luisa1108 <159147642+luisa1108@users.noreply.github.com>
2024-05-15 11:27:53 -0500

    Actualización

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.airbnb.android:lottie:6.4.0")
+    implementation("org.json:json:20210307")
+    implementation ("com.squareup.picasso:picasso:2.71828")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("org.json:json:20210307")
-    implementation ("com.squareup.picasso:picasso:2.71828")
+
+
 }
---
+++ app/src/main/java/com/triana/libreria_/animationActivity.java
@@ -0,0 +1,23 @@
+package com.triana.libreria_;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class animationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(new Intent(animationActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 5000);
+
+    }
+}
