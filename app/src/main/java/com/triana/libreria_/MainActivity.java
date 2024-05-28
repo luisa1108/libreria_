46237410ffc05887f9f7f18e259ab783070ea58c
Triana-619 <145723695+Triana-619@users.noreply.github.com>
2024-05-28 01:08:49 -0500

    Diseño

--- app/src/main/java/com/triana/libreria_/MainActivity.java
+++ app/src/main/java/com/triana/libreria_/MainActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,19 +23,31 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private LibroAdapter adapter;
     private List<Libro> listaLibros;
+    private EditText buscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        buscar= findViewById(R.id.search_box);
         listView = findViewById(R.id.lista_libros);
         listaLibros = new ArrayList<>();
         adapter = new LibroAdapter(this, listaLibros);
         listView.setAdapter(adapter);
 
-        // Iniciar la tarea asíncrona para buscar libros
-        new BuscarLibrosTask().execute("muerte");
+        buscar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE ||
+                        (event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+                    // Iniciar la tarea asíncrona para buscar libros
+                    new BuscarLibrosTask().execute(buscar.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
         // En el método onCreate o después de inicializar tu ListView
         listView.setOnItemClickListener((parent, view, position, id) -> {
             Libro libroSeleccionado = listaLibros.get(position);
@@ -37,8 +55,6 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("libro", libroSeleccionado); // Asegúrate de que Libro implemente Serializable
             startActivity(intent);
         });
-
-
     }
 
     // Clase interna para la tarea asíncrona
@@ -59,6 +75,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
 }
 
